d53f76ce8c430e02c2c3e17ef86ca5b6368af6bc
r2d2 <arturo.galvis@hotmail.com>
2023-02-20 09:56:39 -0500

    main

--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -1,8 +1,16 @@
 import React from "react";
 import "./TodoCounter.css";
 
-function TodoCounter() {
-  return <h2 className="TodoCounter"> Has complentado 2 de 3 Todos</h2>;
+// total y completed reciben totalTodos y completedTodos de app.js. Estamos desestructurando las propiedades pero otra forma de escribir lo mismo poria ser
+/*function TodoCounter(props) {
+  const {total, completed } = props;
+  ...} */
+function TodoCounter({ total, completed }) {
+  return (
+    <h2 className="TodoCounter">
+      Has completado {completed} de {total} Todos
+    </h2>
+  );
 }
 
 export { TodoCounter };
